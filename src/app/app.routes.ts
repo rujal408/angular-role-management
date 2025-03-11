068ac085a11f98b1e3f6ccb8c3063a275a9aebed
rujal408 <rujal408@gmail.com>
2025-03-11 15:27:51 +0545

    add user and role component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RoleComponent } from './role/role.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: 'role', component: RoleComponent },
+];
---
+++ src/app/role/role.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'role',
+  imports: [],
+  templateUrl: './role.component.html',
+  styleUrl: './role.component.scss',
+})
+export class RoleComponent {}
